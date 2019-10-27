3f0ad1ee043775962873803af079bf4c7e9e5f36
Hach-Trykach <dudarev.vlad@gmail.com>
2019-10-27 16:40:49 +0300

    update

--- app/src/main/java/by/ddrvld/countdown/DateActivity.java
+++ app/src/main/java/by/ddrvld/countdown/DateActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
@@ -19,13 +20,14 @@ public class DateActivity extends Activity {
 
     static SharedPreferences settings;
     static final String APP_PREFERENCES = "settings";
+    static final String RANDOMLIFETIME = "randomlifetime";
     static final String YEARS = "years";
     static final String DAYS = "days";
     static final String HOURS = "hours";
     static final String MINS = "mins";
     static final String SECS = "sces";
 
-    Long randomLifeTime;
+    int randomLifeTime;
     Long currentTime = System.currentTimeMillis()/1000;
 
     int fullDays = 364, fullHours = 23, fullMins = 59, fullSecs = 59;
@@ -56,34 +58,72 @@ public class DateActivity extends Activity {
 
         settings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
 
-        if (settings.contains(YEARS) || settings.contains(DAYS) || settings.contains(HOURS) || settings.contains(MINS) || settings.contains(SECS)) {
-            years = settings.getInt(YEARS, 0);// Получаем число из настроек
-            days = settings.getInt(DAYS, 0);// Получаем число из настроек
-            hours = settings.getInt(HOURS, 0);// Получаем число из настроек
-            mins = settings.getInt(MINS, 0);// Получаем число из настроек
-            secs = settings.getInt(SECS, 0);// Получаем число из настроек
+        if (settings.contains(RANDOMLIFETIME)) {
+            randomLifeTime = settings.getInt(RANDOMLIFETIME, 0);
         }
+//        if (settings.contains(YEARS) || settings.contains(DAYS) || settings.contains(HOURS) || settings.contains(MINS) || settings.contains(SECS)) {
+//            years = settings.getInt(YEARS, 0);// Получаем число из настроек
+//            days = settings.getInt(DAYS, 0);// Получаем число из настроек
+//            hours = settings.getInt(HOURS, 0);// Получаем число из настроек
+//            mins = settings.getInt(MINS, 0);// Получаем число из настроек
+//            secs = settings.getInt(SECS, 0);// Получаем число из настроек
+//        }
         else {
-            randomLifeTime = 216000 + (long) (Math.random() * (1527999157L - 216000));
-
-            nowYears = Integer.parseInt(new SimpleDateFormat("yyyy").format(new Date(currentTime * 1000L)));
-            nowDays = Integer.parseInt(new SimpleDateFormat("ddd").format(new Date(currentTime * 1000L)));
-            nowHours = Integer.parseInt(new SimpleDateFormat("hh").format(new Date(currentTime * 1000L)));
-            nowMins = Integer.parseInt(new SimpleDateFormat("mm").format(new Date(currentTime * 1000L)));
-            nowSecs = Integer.parseInt(new SimpleDateFormat("ss").format(new Date(currentTime * 1000L)));
-
-            lifeYears = Integer.parseInt(new SimpleDateFormat("yyyy").format(new Date((currentTime + randomLifeTime) * 1000L)));
-            lifeDays = Integer.parseInt(new SimpleDateFormat("ddd").format(new Date((currentTime + randomLifeTime) * 1000L)));
-            lifeHours = Integer.parseInt(new SimpleDateFormat("hh").format(new Date((currentTime + randomLifeTime) * 1000L)));
-            lifeMins = Integer.parseInt(new SimpleDateFormat("mm").format(new Date((currentTime + randomLifeTime) * 1000L)));
-            lifeSecs = Integer.parseInt(new SimpleDateFormat("ss").format(new Date((currentTime + randomLifeTime) * 1000L)));
-
-            years = lifeYears - nowYears;
-            days = lifeDays - nowDays;
-            hours = lifeHours - nowHours;
-            mins = lifeMins - nowMins;
-            secs = lifeSecs - nowSecs;
+            randomLifeTime = 216000 + (int) (Math.random() * (1577846300L - 216000));
         }
+
+//        randomLifeTime = randomLifeTime + currentTime;
+
+//        mins = secs / 60;
+//        hours = 24 * mins;
+//        days = 365 * hours;
+
+        secs = randomLifeTime % 60;
+        mins = randomLifeTime / 60 % 60;
+        hours = (randomLifeTime / 3600) - (randomLifeTime / 86400) * 24;
+
+//        nowYears = Integer.parseInt(new SimpleDateFormat("yyyy").format(new Date(currentTime * 1000L)));
+//        nowDays = Integer.parseInt(new SimpleDateFormat("ddd").format(new Date(currentTime * 1000L)));
+//        nowHours = Integer.parseInt(new SimpleDateFormat("hh").format(new Date(currentTime * 1000L)));
+//        nowMins = Integer.parseInt(new SimpleDateFormat("mm").format(new Date(currentTime * 1000L)));
+//        nowSecs = Integer.parseInt(new SimpleDateFormat("ss").format(new Date(currentTime * 1000L)));
+//
+//        lifeYears = Integer.parseInt(new SimpleDateFormat("yyyy").format(new Date(randomLifeTime * 1000L)));
+//        lifeDays = Integer.parseInt(new SimpleDateFormat("ddd").format(new Date(randomLifeTime * 1000L)));
+//        lifeHours = Integer.parseInt(new SimpleDateFormat("hh").format(new Date(randomLifeTime * 1000L)));
+//        lifeMins = Integer.parseInt(new SimpleDateFormat("mm").format(new Date(randomLifeTime * 1000L)));
+//        lifeSecs = Integer.parseInt(new SimpleDateFormat("ss").format(new Date(randomLifeTime * 1000L)));
+//
+////        years = Integer.parseInt(new SimpleDateFormat("yyyy").format(new Date(randomLifeTime * 1000L)));
+////        days = Integer.parseInt(new SimpleDateFormat("ddd").format(new Date(randomLifeTime * 1000L)));
+////        hours = Integer.parseInt(new SimpleDateFormat("hh").format(new Date(randomLifeTime * 1000L)));
+////        mins = Integer.parseInt(new SimpleDateFormat("mm").format(new Date(randomLifeTime * 1000L)));
+////        secs = Integer.parseInt(new SimpleDateFormat("ss").format(new Date(randomLifeTime * 1000L)));
+//
+//        years = lifeYears - nowYears;
+//        days = lifeDays - nowDays;
+//        hours = lifeHours - nowHours;
+//        mins = lifeMins - nowMins;
+//        secs = lifeSecs - nowSecs;
+
+        Log.i("nowYears", "" + nowYears);
+        Log.i("lifeYears", "" + lifeYears);
+        Log.i("years", "" + years);
+        Log.i("nowDays", "" + nowDays);
+        Log.i("lifeDays", "" + lifeDays);
+        Log.i("days", "" + days);
+        Log.i("nowHours", "" + nowHours);
+        Log.i("lifeHours", "" + lifeHours);
+        Log.i("hours", "" + hours);
+        Log.i("nowMins", "" + nowMins);
+        Log.i("lifeMins", "" + lifeMins);
+        Log.i("mins", "" + mins);
+        Log.i("nowSecs", "" + nowSecs);
+        Log.i("lifeSecs", "" + lifeSecs);
+        Log.i("secs", "" + secs);
+
+        Log.i("randomLifeTime", "" + randomLifeTime);
+
         setValues();
     }
 
@@ -135,19 +175,29 @@ public class DateActivity extends Activity {
                         if(secs >= 10) tvSec.setText("" + secs);
                         else tvSec.setText("0" + secs);
 
-                        Log.d("days", "" + days);
-
                         if(end) theEnd();
                     }
                 });
             }
-        }, 0, 1);
+        }, 0, 1000);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+//
+//        String givenDateString = String.format("%d%d%d%d%d", years, days, hours, mins, secs);
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyyddHHmmss");
+//        try {
+//            Date mDate = sdf.parse(givenDateString);
+//            long timeInMilliseconds = mDate.getTime();
+//            System.out.println("Date in milli :: " + timeInMilliseconds);
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+
         SharedPreferences.Editor editor = settings.edit();
+        editor.putLong(RANDOMLIFETIME, randomLifeTime);
         editor.putInt(YEARS, years);
         editor.putInt(DAYS, days);
         editor.putInt(HOURS, hours);
