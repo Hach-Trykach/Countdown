e09efb7a5b162125237c125ca4b85fdfe6d77779
Mr.X <dudarev.vlad@gmail.com>
2020-09-18 09:27:49 +0300

    update

--- app/src/main/java/by/ddrvld/countdowndeathapp/Update.java
+++ app/src/main/java/by/ddrvld/countdowndeathapp/Update.java
@@ -47,18 +47,19 @@ public class Update extends AppWidgetProvider {
         for(int i=0; i<appWidgetIds.length; i++){
             int currentWidgetId = appWidgetIds[i];
 
-            //Делаем простой http запрос на указанную ссылку и выполняем по ней переход:
-            String url = "https://ddrvld.com";
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.setData(Uri.parse(url));
-
-            //Определяем два обязательных объекта класса PendingIntent и RemoteViews:
-            PendingIntent pending = PendingIntent.getActivity(context, 0,intent, 0);
+//            //Делаем простой http запрос на указанную ссылку и выполняем по ней переход:
+//            String url = "https://ddrvld.com";
+//            Intent intent = new Intent(Intent.ACTION_VIEW);
+//            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//            intent.setData(Uri.parse(url));
+//
+//            //Определяем два обязательных объекта класса PendingIntent и RemoteViews:
+//            PendingIntent pending = PendingIntent.getActivity(context, 0,intent, 0);
             RemoteViews views = new RemoteViews(context.getPackageName(),R.layout.appwidget_provider_layout);
-
-            //Настраиваем обработку клика по добавлению виджета:
-            views.setOnClickPendingIntent(R.id.button, pending);
+//
+//            //Настраиваем обработку клика по добавлению виджета:
+//            views.setOnClickPendingIntent(R.id.button, pending);
+            views.setTextViewText(R.id.textView,"Обновился");
             appWidgetManager.updateAppWidget(currentWidgetId,views);
             Toast.makeText(context, "Виджет добавлен", Toast.LENGTH_SHORT).show();
         }
