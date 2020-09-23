214cacba8b7f0a5b519ff0f5ab1af6a94dc7fe8f
Mr.X <dudarev.vlad@gmail.com>
2020-09-23 19:02:56 +0300

    uodate

--- app/src/main/java/by/ddrvld/countdowndeathapp/Update.java
+++ app/src/main/java/by/ddrvld/countdowndeathapp/Update.java
@@ -1,41 +1,39 @@
 package by.ddrvld.countdowndeathapp;
 
-import android.Manifest;
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.content.ComponentName;
 import android.content.SharedPreferences;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
-import android.preference.PreferenceManager;
-import android.telephony.TelephonyManager;
 import android.util.Log;
-import android.view.View;
 import android.widget.RemoteViews;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
-import static android.content.Context.TELEPHONY_SERVICE;
-
 public class Update extends AppWidgetProvider {
 
-    RemoteViews remoteViews;
+    private static int currentWidgetId;
+    private static RemoteViews remoteViews;
     static SharedPreferences settings;
     static final String APP_PREFERENCES = "settings";
     static final String DATE_OF_DEATH = "randomlifetime";
-    long timerTime;
-    long years, days, hours, mins, secs;
+    private static long timerTime;
+    private static long years, days, hours, mins, secs;
+    private PendingIntent service = null;
+
+    private static final String SYNC_CLICKED    = "btcwidget_update_action";
+    private static final String WAITING_MESSAGE = "Wait for BTC price";
+    public static final int httpsDelayMs = 300;
 
     final String LOG_TAG = "myLogs";
 
@@ -44,13 +42,6 @@ public class Update extends AppWidgetProvider {
         super.onEnabled(context);
         Log.d(LOG_TAG, "onEnabled");
     }
-
-//    @Override
-//    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
-//                         int[] appWidgetIds) {
-//        super.onUpdate(context, appWidgetManager, appWidgetIds);
-//        Log.d(LOG_TAG, "onUpdate " + Arrays.toString(appWidgetIds));
-//    }
 
     @Override
     public void onDeleted(Context context, int[] appWidgetIds) {
@@ -66,42 +57,100 @@ public class Update extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        for(int i=0; i<appWidgetIds.length; i++){
-//            int currentWidgetId = appWidgetIds[i];
+        RemoteViews remoteViews;
+        ComponentName watchWidget;
 
-            settings = context.getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+        remoteViews = new RemoteViews(context.getPackageName(), R.layout.appwidget_provider_layout);
+        watchWidget = new ComponentName(context, Update.class);
 
-            DateFormat format = SimpleDateFormat.getTimeInstance(SimpleDateFormat.MEDIUM, Locale.getDefault());
-            CharSequence text = format.format(new Date());
+        //при клике на виджет в систему отсылается вот такой интент, описание метода ниже
+        remoteViews.setOnClickPendingIntent(R.id.linear_layout, getPendingSelfIntent(context, SYNC_CLICKED));
+        appWidgetManager.updateAppWidget(watchWidget, remoteViews);
 
-            Intent intent = new Intent(context, Update.class);
-            PendingIntent pendingIntent = PendingIntent.getService(context, 0, intent, 0);
-
-            remoteViews = new RemoteViews(context.getPackageName(), R.layout.appwidget_provider_layout);
-            remoteViews.setOnClickPendingIntent(R.id.linearLayout, pendingIntent);
-
-            long date_of_death = 0;
-            long currentTime = System.currentTimeMillis() / 1000;
-            if (settings.contains(DATE_OF_DEATH)) {
-                date_of_death = settings.getLong(DATE_OF_DEATH, 0);
-            }
-            timerTime = date_of_death - currentTime;
-
-            years = timerTime / 31536000;
-            days = timerTime / 86400 % 365;
-            hours = timerTime / 3600 % 24;
-            mins = timerTime / 60 % 60;
-            secs = timerTime % 60;
-
-            updateUI(context);
-
-            // Tell the widget manager
-            appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
-            Toast.makeText(context, "Виджет обновлён", Toast.LENGTH_SHORT).show();
+        //обновление всех экземпляров виджета
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
         }
     }
 
-    private void updateUI(Context context) {
+    //этот метод выполняется, когда пора обновлять виджет
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId)  {
+        currentWidgetId = appWidgetId;
+        settings = context.getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+
+        DateFormat format = SimpleDateFormat.getTimeInstance(SimpleDateFormat.MEDIUM, Locale.getDefault());
+        CharSequence text = format.format(new Date());
+
+        Intent intent = new Intent(context, Update.class);
+        PendingIntent pendingIntent = PendingIntent.getService(context, 0, intent, 0);
+
+        remoteViews = new RemoteViews(context.getPackageName(), R.layout.appwidget_provider_layout);
+        remoteViews.setOnClickPendingIntent(R.id.linear_layout, pendingIntent);
+
+        long date_of_death = 0;
+        long currentTime = System.currentTimeMillis() / 1000;
+        if (settings.contains(DATE_OF_DEATH)) {
+            date_of_death = settings.getLong(DATE_OF_DEATH, 0);
+        }
+        timerTime = date_of_death - currentTime;
+
+        years = timerTime / 31536000;
+        days = timerTime / 86400 % 365;
+        hours = timerTime / 3600 % 24;
+        mins = timerTime / 60 % 60;
+        secs = timerTime % 60;
+
+        updateUI(context);
+
+        // Tell the widget manager
+        appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+        Toast.makeText(context, "Виджет обновлён", Toast.LENGTH_SHORT).show();
+    }
+
+    //создание интента
+    protected PendingIntent getPendingSelfIntent(Context context, String action) {
+        Intent intent = new Intent(context, getClass());
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(context, 0, intent, 0);
+    }
+
+    //этот метод ловит интенты, срабатывает когда интент создан нажатием на виджет и
+    //запускает обновление виджета
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        super.onReceive(context, intent);
+
+        if (SYNC_CLICKED.equals(intent.getAction())) {
+
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+
+            RemoteViews remoteViews;
+            ComponentName watchWidget;
+
+            remoteViews = new RemoteViews(context.getPackageName(), R.layout.appwidget_provider_layout);
+            watchWidget = new ComponentName(context, Update.class);
+
+            remoteViews.setTextViewText(R.id.yrs, WAITING_MESSAGE);
+
+            //updating widget
+            appWidgetManager.updateAppWidget(watchWidget, remoteViews);
+        }
+        final AlarmManager manager=(AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        final Calendar startTime = Calendar.getInstance();
+        startTime.set(Calendar.MINUTE, 0);
+        startTime.set(Calendar.SECOND, 0);
+        startTime.set(Calendar.MILLISECOND, 0);
+
+        final Intent i = new Intent(context, WidgetUpdateService.class);
+        if (service == null)
+        {
+            service = PendingIntent.getService(context, 0, i, PendingIntent.FLAG_CANCEL_CURRENT);
+        }
+        manager.setRepeating(AlarmManager.RTC,startTime.getTime().getTime(),1000,service);
+    }
+
+    private static void updateUI(Context context) {
         if (years >= 10) remoteViews.setTextViewText(R.id.yrs, String.valueOf(years));
         else remoteViews.setTextViewText(R.id.yrs, "0" + years);
 
---
+++ app/src/main/java/by/ddrvld/countdowndeathapp/WidgetUpdateService.java
@@ -0,0 +1,34 @@
+package by.ddrvld.countdowndeathapp;
+
+import android.app.Service;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class WidgetUpdateService  extends Service {
+
+    public WidgetUpdateService() {
+    }
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        updateInfoWidget();
+        return super.onStartCommand(intent, flags, startId);
+    }
+    private void updateInfoWidget()
+    {//Обновление виджета
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        int[] ids = appWidgetManager.getAppWidgetIds(new ComponentName(this.getApplicationContext().getPackageName(), Update.class.getName()));
+        Update.updateAppWidget(this.getApplicationContext(),appWidgetManager,ids[0]);
+    }
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
