ea6534c5d908c7c9e59abb3e54e8be2d3331eed7
Mr.X <dudarev.vlad@gmail.com>
2020-11-08 22:51:26 +0300

    add replaceAll (\n\n\n)

--- app/src/main/java/by/ddrvld/countdowndeathapp/ChatActivity.java
+++ app/src/main/java/by/ddrvld/countdowndeathapp/ChatActivity.java
@@ -124,10 +124,6 @@ public class ChatActivity extends AppCompatActivity {
 
                 String text = emojiconEditText.getText().toString();
 
-                if(text.isEmpty()) return;
-//                if(text.equals(" ")) return;
-//                if(text.equals("\n")) return;
-//                if(text.length() < 2) return;
                 while (text.charAt(0) == ' ' || text.charAt(0) == '\n') {
                     try {
                         text = text.substring(1); // Удаляем первый символ
@@ -139,24 +135,32 @@ public class ChatActivity extends AppCompatActivity {
                 }
                 while (text.charAt(text.length() -1) == ' ' || text.charAt(text.length() -1) == '\n') {
                     try {
-                        text = text.substring(0, text.length() -1); // Удаляем последний  символ
+                        text = text.substring(0, text.length() -1); // Удаляем последний символ
                         Log.d("", "SUBSTRING");
                     }
                     catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
-//                String msg = "";
-//                if (text.indexOf(" ") != -1) {
-//                    msg = "элемент есть в тексте";
-//                }
-//                else {
-//                    msg = "элемента в тексте нет";
-//                }
-//                Snackbar.make(findViewById(android.R.id.content), msg, Snackbar.LENGTH_SHORT).show();
-                FirebaseDatabase.getInstance().getReference().push().setValue(
-                        new Message(FirebaseAuth.getInstance().getCurrentUser().getDisplayName(), text));
-                emojiconEditText.setText("");
+                while (text.contains("\n\n\n")) {
+                    try {
+                        text = text.replaceAll("\n\n\n", "");
+//                        Snackbar.make(findViewById(android.R.id.content), "Слишком много отступов", Snackbar.LENGTH_SHORT).show();
+                    }
+                    catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+//                if(text.equals(" ")) return;
+//                if(text.equals("\n")) return;
+//                if(text.length() < 2) return;
+                if(text.isEmpty()) return;
+                else {
+                    FirebaseDatabase.getInstance().getReference().push().setValue(
+                            new Message(FirebaseAuth.getInstance().getCurrentUser().getDisplayName(), text));
+                    emojiconEditText.setText("");
+                }
             }
         });
         displayAllMessages();
