3df226f52f676164ad73b88d237285e81627a2d4
Mr.X <dudarev.vlad@gmail.com>
2020-11-09 21:15:07 +0300

    update

--- app/src/main/java/by/ddrvld/countdowndeathapp/ChatActivity.java
+++ app/src/main/java/by/ddrvld/countdowndeathapp/ChatActivity.java
@@ -1,11 +1,19 @@
 package by.ddrvld.countdowndeathapp;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.text.InputFilter;
 import android.text.Spanned;
 import android.text.format.DateFormat;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
@@ -42,6 +50,11 @@ public class ChatActivity extends AppCompatActivity {
 
 //    private Drawer drawerResult;
 
+    private Message selectedListItem; //hold selected item
+    private boolean selectMode = false;
+    private int selectItemSize;
+    private Toolbar toolbar;
+
     //===============================initializeAuthVariables========================================
     final String TAG = "PhoneAuthActivity";
 
@@ -49,13 +62,16 @@ public class ChatActivity extends AppCompatActivity {
 
     private ListView listOfMessages;
     private ProgressBar circular_progress;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+//        mAuth = FirebaseAuth.getInstance();
+
         if(FirebaseAuth.getInstance().getCurrentUser() == null) {
-            Snackbar.make(activity_chat, "Вы не авторизованы 2", Snackbar.LENGTH_LONG).show();
+            Snackbar.make(activity_chat, "Вы не авторизованы..", Snackbar.LENGTH_LONG).show();
         }
         else {
             onCreateChat();
@@ -81,10 +97,10 @@ public class ChatActivity extends AppCompatActivity {
     private void onCreateChat() {
         setContentView(R.layout.activity_chat);
 
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        if (toolbar != null) {
-//            setSupportActionBar(toolbar);
-//        }
+        toolbar = findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
 //        initializeDrawer(toolbar);
 
         activity_chat = findViewById(R.id.activity_chat);
@@ -99,6 +115,8 @@ public class ChatActivity extends AppCompatActivity {
         //показываем View загрузки
         circular_progress.setVisibility(View.VISIBLE);
         listOfMessages.setVisibility(View.INVISIBLE);
+
+        registerForContextMenu(listOfMessages);
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -144,7 +162,7 @@ public class ChatActivity extends AppCompatActivity {
                 }
                 while (text.contains("\n\n\n")) {
                     try {
-                        text = text.replaceAll("\n\n\n", "");
+                        text = text.replaceAll("\n\n\n", "\n\n");
 //                        Snackbar.make(findViewById(android.R.id.content), "Слишком много отступов", Snackbar.LENGTH_SHORT).show();
                     }
                     catch (Exception e) {
@@ -155,7 +173,8 @@ public class ChatActivity extends AppCompatActivity {
 //                if(text.equals(" ")) return;
 //                if(text.equals("\n")) return;
 //                if(text.length() < 2) return;
-                if(text.isEmpty()) return;
+                if(text.isEmpty()) {
+                }
                 else {
                     FirebaseDatabase.getInstance().getReference().push().setValue(
                             new Message(FirebaseAuth.getInstance().getCurrentUser().getDisplayName(), text));
@@ -164,6 +183,106 @@ public class ChatActivity extends AppCompatActivity {
             }
         });
         displayAllMessages();
+
+
+        circular_progress = findViewById(R.id.circular_progress);
+        listOfMessages = findViewById(R.id.list_of_messages);
+
+//        initFirebase();
+
+        listOfMessages.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(selectMode) {
+                    if(listOfMessages.isItemChecked(i) /*&& itemOldClickId != i*/) {
+                        view.setBackground(getResources().getDrawable(R.drawable.yes_no_buttons));
+
+                        view.setSelected(true);
+                        selectItemSize++;
+
+                        Message message = (Message) adapterView.getItemAtPosition(i);
+                        selectedListItem = message;
+
+                        toolbar.setTitle("" + selectItemSize);
+                    }
+                    else {
+                        view.setBackground(getResources().getDrawable(R.drawable.yes_no_buttons));
+                        view.setSelected(false);
+                        if(selectItemSize > 0) {
+                            selectItemSize--;
+                            toolbar.setTitle("" + selectItemSize);
+                        }
+                        if (selectItemSize == 0){
+                            deselectAllItems();
+                        }
+                    }
+                }
+                else {
+                    Message message = (Message) adapterView.getItemAtPosition(i);
+//                    openInWindow(message);
+                    listOfMessages.setItemChecked(i, false);
+                }
+            }
+        });
+        listOfMessages.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(!selectMode) {
+                    createVibration(30);
+//                    Notes note = (Notes) adapterView.getItemAtPosition(i);
+//                    selectedListItem = note;
+//                    adapterView.setSelected(true);
+                    selectMode = true;
+//                    view.setSelected(true);
+//                    listView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
+                    return false;
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        getMenuInflater().inflate(R.menu.context_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.option_1:
+                Snackbar.make(findViewById(android.R.id.content), "Выбран Option_1", Snackbar.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    private void deselectAllItems() {
+        createVibration(15);
+        selectItemSize = 0;
+        selectedListItem = null;
+//        adapterView.setSelected(false);
+        selectMode = false;
+        toolbar.setTitle(R.string.app_name);
+    }
+
+    private void createVibration(int duration) {
+        Vibrator vibration = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            vibration.vibrate(VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            //deprecated in API 26
+            vibration.vibrate(duration);
+        }
     }
 
     @Override
@@ -171,7 +290,7 @@ public class ChatActivity extends AppCompatActivity {
 //        if(drawerResult != null && drawerResult.isDrawerOpen()) {
 //            drawerResult.closeDrawer();
 //        } else {
-            super.onBackPressed();
+        super.onBackPressed();
 //        }
     }
 
@@ -224,6 +343,7 @@ public class ChatActivity extends AppCompatActivity {
 //                .build();
 //    }
 
+    //        connectedRef.child("users").child(mAuth.getUid()).addValueEventListener(new ValueEventListener() {
     private void displayAllMessages() {
         DatabaseReference connectedRef = FirebaseDatabase.getInstance().getReference(".info/connected");
         connectedRef.addValueEventListener(new ValueEventListener() {
