06eee5f4a06d0ae05a4754e44e294c24272a5482
Dudarev_Vlad <dudarev.vlad@gmail.com>
2021-07-29 14:46:19 +0300

    add chat

--- app/src/main/java/by/ddrvld/countdowndeathapp/MainActivity.java
+++ app/src/main/java/by/ddrvld/countdowndeathapp/MainActivity.java
@@ -59,6 +59,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.play.core.review.ReviewInfo;
@@ -803,24 +804,24 @@ public class MainActivity extends AppCompatActivity implements BillingProcessor.
                 return true;
             }
             else if(drawerItem.getIdentifier() == BTN_CHANGE_UR_FATE) {
-//                if(user == null) {
-//                    // Configure Google Sign In
-//                    GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//                            .requestIdToken(getString(R.string.default_web_client_id))
-//                            .requestEmail()
-//                            .requestProfile()
-//                            .build();
-//
-//                    // Build a GoogleSignInClient with the options specified by gso.
-//                    mGoogleSignInClient = GoogleSignIn.getClient(getBaseContext(), gso);
-//
-//                    Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-//                    startActivityForResult(signInIntent, RC_SIGN_IN);
-//                }
-//                else {
-//                    Intent intent = new Intent(MainActivity.this, ChatActivity.class);
-//                    startActivity(intent);
-//                }
+                if(user == null) {
+                    // Configure Google Sign In
+                    GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                            .requestIdToken(getString(R.string.default_web_client_id))
+                            .requestEmail()
+                            .requestProfile()
+                            .build();
+
+                    // Build a GoogleSignInClient with the options specified by gso.
+                    mGoogleSignInClient = GoogleSignIn.getClient(getBaseContext(), gso);
+
+                    Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                    startActivityForResult(signInIntent, RC_SIGN_IN);
+                }
+                else {
+                    Intent intent = new Intent(MainActivity.this, ChatActivity.class);
+                    startActivity(intent);
+                }
 //                Snackbar.make(findViewById(android.R.id.content), getResources().getString(R.string.coming_soon), Snackbar.LENGTH_SHORT).show();
                 mBillingProcessor.purchase(MainActivity.this, CHANGE_YOUR_FATE);
                 return true;
@@ -850,7 +851,7 @@ public class MainActivity extends AppCompatActivity implements BillingProcessor.
                     Intent intent = new Intent(MainActivity.this, ChatActivity.class);
                     startActivity(intent);
                 }
-                Snackbar.make(findViewById(android.R.id.content), getResources().getString(R.string.coming_soon), Snackbar.LENGTH_SHORT).show();
+//                Snackbar.make(findViewById(android.R.id.content), getResources().getString(R.string.coming_soon), Snackbar.LENGTH_SHORT).show();
                 return true;
             }
             return false;
@@ -1571,24 +1572,26 @@ public class MainActivity extends AppCompatActivity implements BillingProcessor.
 //        startActivityForResult(signInIntent, RC_SIGN_IN);
 //    }
 
-//    @Override
-//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-//        if (requestCode == RC_SIGN_IN) {
-//            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-//            try {
-//                // Google Sign In was successful, authenticate with Firebase
-//                GoogleSignInAccount account = task.getResult(ApiException.class);
-//                firebaseAuthWithGoogle(account);
-//            } catch (ApiException e) {
-//                // Google Sign In failed, update UI appropriately
-//                Log.w("TAG", "Google sign in failed", e);
-//                // ...
-//            }
-//        }
-//    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!mBillingProcessor.handleActivityResult(requestCode, resultCode, data)) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            com.google.android.gms.tasks.Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("TAG", "Google sign in failed", e);
+                // ...
+            }
+        }
+    }
 
     private void firebaseAuthWithGoogle(final GoogleSignInAccount acct) {
         Log.d("TAG", "firebaseAuthWithGoogle:" + acct.getId());
@@ -1675,6 +1678,12 @@ public class MainActivity extends AppCompatActivity implements BillingProcessor.
                 .withIcon(android.R.drawable.ic_menu_share)
                 .withIdentifier(BTN_SHARE);
 
+        final PrimaryDrawerItem chatDrawerItem = new PrimaryDrawerItem()
+                .withName("Chat")
+                .withTextColorRes(R.color.white)
+                .withIcon(android.R.drawable.ic_dialog_email)
+                .withIdentifier(BTN_CHAT);
+
         PrimaryDrawerItem disableAdsDrawerItem;
         if(!noAds) {
             disableAdsDrawerItem = new PrimaryDrawerItem()
@@ -1703,6 +1712,7 @@ public class MainActivity extends AppCompatActivity implements BillingProcessor.
                 .addDrawerItems(changeYourFateDrawerItem)
                 .addDrawerItems(disableAdsDrawerItem)
                 .addDrawerItems(shareDrawerItem)
+                .addDrawerItems(chatDrawerItem)
                 //            .addStickyDrawerItems(initializeStickyDrawerItems())
                 .withOnDrawerItemClickListener(onClicksLis)
                 //            .withAccountHeader(accountHeader)
@@ -1933,13 +1943,6 @@ public class MainActivity extends AppCompatActivity implements BillingProcessor.
     private void showMsg(String text) {
 //        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
         Log.d("showMsg", text);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (!mBillingProcessor.handleActivityResult(requestCode, resultCode, data)) {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
     }
 
     private void readFromDatabase() {
